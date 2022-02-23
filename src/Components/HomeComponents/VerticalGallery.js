9a77fd59668669032bcecdfa6ad7bdddea193ea2
coderashraf <ashrafulbnt@gmail.com>
2022-02-23 16:13:26 +0530

    23feb1613

--- src/Components/HomeComponents/VerticalGallery.js
+++ src/Components/HomeComponents/VerticalGallery.js
@@ -9,7 +9,7 @@ const VerticalGallery = () => {
   const [techLatestBlog,setTechLatestBlog]=useState([])
   useEffect(() => {
     //api call to get the last post of technology category
-    axios.get(`https://blog-app-backend-coderashraf.herokuapp.com/api/latestTechBlog/`).then((res)=>{
+    axios.get(`https://blog-app-backend-coderashraf.herokuapp.com/api/latestTechBlog`).then((res)=>{
       setTechLatestBlog(res.data)
     })
   },[]);
